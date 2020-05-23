bc67d509d74f62b45dd4d0174fcc21a4d267c003
xionghao <xionghao930316@gmail.com>
2020-05-23 18:22:25 +0800

    update network

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,13 +1,6 @@
 
 import 'package:dio/dio.dart';
 
-abstract class BaseData {
-  final int code;
-  final String message;
-
-  BaseData(this.code, this.message);
-}
-
 enum NetworkStatus {
   initial, loading, success, failure
 }
@@ -55,36 +48,4 @@ Future<Resource<T>> networkStrategy<T>(ApiResponse response) async {
   } else {
     return Resource(NetworkStatus.failure, null, response.error);
   }
-}
-
-/// 处理业务状态码
-Future<T> handleResponse<T>(Future<Resource<T>> future, Function(int) statusCall) {
-  return Stream.fromFuture(future).map((r) {
-    if (r.status == NetworkStatus.success) {
-      if (r.data is BaseData) {
-        final base = r.data as BaseData;
-        statusCall(base.code);
-        switch(base.code) {
-          case 0:
-            // 请求成功
-            return r.data;
-          case 200:
-            // 登陆失效
-            break;
-          default:
-
-        }
-      } else {
-        throw Exception("data must extend BaseData");
-      }
-    } else {
-      // 显示错误信息
-//      showToast(r.message);
-    }
-    return null;
-  }).first;
-}
-
-test() {
-
 }
