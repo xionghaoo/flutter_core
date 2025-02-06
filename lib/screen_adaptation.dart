119ae8afb354ea0bed19e358c9685d23b26eb236
xionghaoo <xionghaovo@gmail.com>
2025-02-06 15:34:38 +0800

    add dp ext

--- lib/screen_adaptation.dart
+++ lib/screen_adaptation.dart
@@ -22,3 +22,12 @@ double xdp(double size) {
   return size * screenWidth! / uiScreenWidth;
 }
 
+extension DpInt on int {
+  double dp() {
+    if (scale == null || screenWidth == null) {
+      throw Exception("请先初始化屏幕缩放因子和实际屏幕宽度");
+    }
+    return this * screenWidth! / uiScreenWidth;
+  }
+}
+
